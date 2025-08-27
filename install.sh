90cd199d370d1a8baa01847badb2a0465aace615
Daniel <lewdaniel2@gmail.com>
2025-08-27 17:19:04 -0400

    install script auto replaced shebang with home dir and removed existing pdf venv if any

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 
 python -m venv pdf-venv
 pdf-venv/bin/pip install -r requirements.txt
+sed -i "1s|^#!.*|#!$HOME/.local/share/pdf-venv/bin/python|" pdf.py
 chmod +x pdf.py
 mv pdf.py ~/.local/bin/pdf
+rm -rf ~/.local/share/pdf-venv
 mv pdf-venv ~/.local/share
 cd ..
 rm -rf PDFprocessor
